b3453b7f6da69921598fa36805b5c72882335640
Exus Altimus <ealtimus@chainbridgetech.com>
2018-08-29 14:27:44 -0400

    examples

---
+++ examples/StockAnalyzer/StockAnalyzer/AlphaVantageStockDataQuerier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exus.AlphaVantage;
+using Exus.AlphaVantage.QueryResults;
+using StockAnalyzer.Models;
+
+namespace StockAnalyzer
+{
+
+    public class AlphaVantageStockDataQuerier
+    {
+        public readonly IApiQuerier _apiQuerier;
+
+        public AlphaVantageStockDataQuerier(IApiQuerier apiQuerier)
+        {
+            _apiQuerier = apiQuerier;
+        }
+
+        public async Task<TimeSeriesIntradayQueryResult> GetCurrentStockPrice(string symbol)
+        {
+            //var query = new Exus.AlphaVantage.Queries.TimeSeriesIntradayQuery
+            //{
+            //    Symbol = symbol
+            //};
+
+            var query = new Exus.AlphaVantage.ApiQuery<dynamic>(new Dictionary<string, string>()
+            {
+                { "function", "CURRENCY_EXCHANGE_RATE" },
+                { "from_currency", "USD" },
+                { "to_currency", "BTC" }
+            });
+
+            var result = await _apiQuerier.Query(query);
+
+            return result;
+        }
+    }
+}
---
+++ examples/StockAnalyzer/StockAnalyzer/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StockAnalyzer.Models;
+
+namespace StockAnalyzer.Controllers
+{
+    public class HomeController : Controller
+    {
+        //https://www.alphavantage.co/support/#api-key
+        private readonly AlphaVantageStockDataQuerier _stockDataQuerier;
+
+        public HomeController(AlphaVantageStockDataQuerier stockDataQuerier)
+        {
+            _stockDataQuerier = stockDataQuerier;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _stockDataQuerier.GetCurrentStockPrice("MSFT");
+            return View(result);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
